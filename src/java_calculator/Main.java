7d05b5a581d7dd61b9ca43ee3aaecaa8c7f3fd99
alexMillerVince <alex.miller.vince@gmail.com>
2017-08-08 15:04:14 +0200

    set scene size and turn non-resizeable

--- src/java_calculator/Main.java
+++ src/java_calculator/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Calculator by Game Of Codes");
+        primaryStage.setScene(new Scene(root, 400, 450));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
