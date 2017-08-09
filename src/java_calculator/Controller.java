d2a4c0dc3bc260a8d71574d9b04aef26925fc9bb
Daniel Eros <drerosdaniel@gmail.com>
2017-08-09 10:10:13 +0200

    implement prototype for button

--- src/java_calculator/Controller.java
+++ src/java_calculator/Controller.java
@@ -1,4 +1,23 @@
 package java_calculator;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent e) {
+        // cast ActionEvent type variable called e to Button type & call getSource().getText() on it
+        // to retrieve its text value
+        String value = ((Button) e.getSource()).getText();
+        System.out.println(value);
+    }
+
+
+
 }
