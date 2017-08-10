669e67e3af12539bdc4cc4c19940f97ccd2682e1
alexMillerVince <alex.miller.vince@gmail.com>
2017-08-10 22:17:00 +0200

    added stylesheet to scene

--- src/java_calculator/Main.java
+++ src/java_calculator/Main.java
@@ -5,18 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        File css = new File("style.css");
         primaryStage.setTitle("Calculator by Game Of Codes");
-        primaryStage.setScene(new Scene(root, 466, 450));
+        Scene scene = new Scene(root, 466, 450);
+        scene.getStylesheets().add("file:///" + css.getAbsolutePath().replace("\\", "/"));
+        primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
