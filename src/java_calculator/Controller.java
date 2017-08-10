daec0ed2409a1bb2327d4dfdabf56a5a0ad7d3a1
BenceZambo <bence.zambo96@gmail.com>
2017-08-10 23:47:20 +0200

    project commented out

--- src/java_calculator/Controller.java
+++ src/java_calculator/Controller.java
@@ -11,13 +11,22 @@ import java.util.TimerTask;
 
 
 public class Controller {
+    // Default variables
     String number1 = "";
     String number2 = "";
     String operator = "";
+
+    // Contains the last result
     String memory;
+
+    // Decide if the number has got a decimal point or not
     Boolean isDecimalNum1 = true;
     Boolean isDecimalNum2 = true;
+
+    // Decides if the user would like to count with the result, or not
     Boolean overwriteResult = false;
+
+
     Boolean isMemory = true;
     Boolean isResult = false;
 
@@ -31,6 +40,8 @@ public class Controller {
         String value = ((Button) e.getSource()).getText();
         String[] digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         String[] operators = {"/", "+", "-", "*", "MOD", "xʸ"};
+
+        // The user could put prefix mark only at the beginning of a number
         if (value.equals("+/-")  && (number1.equals("") || number1.equals("-") || overwriteResult)){
             if(number1.equals("-")){
                 number1 = "";
@@ -44,6 +55,8 @@ public class Controller {
             }
             TextField.setText(number1);
         }
+
+        // Typing the second number is only allowed if there are the first number, and an operator
         if (value.equals("+/-") && operator != "" && (number2.equals("") || number2.equals("-"))){
             if(number2.equals("-")){
                 number2 = "";
@@ -52,6 +65,9 @@ public class Controller {
             }
             TextField.setText(number1 + operator + number2);
         }
+
+        // If the user hit the =EGGYELLŐ= button, the it call the handleCalculation function, and sets the
+        // variables empty, or stores the result
         if (value.equals("=EGGYELLŐ=") && number2 != "") {
             String toTextField = String.valueOf(handleCalculation(number1, number2, operator));
             TextField.setText(toTextField);
@@ -64,6 +80,8 @@ public class Controller {
             isMemory = true;
             isResult = true;
         }
+
+        // Set all the variables to empty
         if (value.equals("C")) {
             number1 = "";
             number2 = "";
@@ -72,6 +90,8 @@ public class Controller {
             isDecimalNum1 = true;
             isDecimalNum2 = true;
         }
+
+        //Put a zero before the decimal point, it's just style
         if (Arrays.asList(operators).contains(value) && !number1.equals("") && number2.equals("")) {
             overwriteResult = false;
             if(number1.substring(number1.length() - 1).equals(".")) {
@@ -80,10 +100,14 @@ public class Controller {
             operator = value;
             TextField.setText(number1 + operator);
         }
+
+        // Sets number2, if number1 and an operator are exist
         if (Arrays.asList(digits).contains(value) && operator != "") {
             number2 += value;
             TextField.setText(number1 + operator + number2);
         }
+
+        // Sets number1 variable
         if (Arrays.asList(digits).contains(value) && operator == "") {
             if (overwriteResult){
                 number1 = "";
@@ -92,16 +116,22 @@ public class Controller {
             number1 += value;
             TextField.setText(number1);
         }
+
+        // It allows only one decimal point to number1
         if (value.equals(".") && isDecimalNum1 && operator == "") {
             number1 += (number1.equals("") ? ("0" + value) : value);
             isDecimalNum1 = false;
             TextField.setText(number1);
         }
+
+        // It allows only one decimal point to number2
         if (value.equals(".") && isDecimalNum2 && operator != "") {
             number2 += (number2.equals("") ? ("0" + value) : value);
             isDecimalNum2 = false;
             TextField.setText(number1 + operator + number2);
         }
+
+        // Write the result to a variable called "memory"
         if (value.equals("M") && isResult && isMemory) {
             Timer timer = new Timer();
             memory = number1;
